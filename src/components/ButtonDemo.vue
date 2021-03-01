477c2ed3439c15778618cf884c40996e4f1db96a
zdxzdxzdx <769371987@qq.com>
2021-03-01 17:19:13 +0800

    支持theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,16 +2,23 @@
     <div> Button示例</div>
     <h1>示例1</h1>
     <div>
-       <Button>我是按钮</Button>
+       <Button theme = "button"></Button>
+       <Button theme = "link"></Button>
+       <Button theme = "text"></Button>
     </div>
 </template>
 
 <script lang="ts">
     import Button from '../lib/Button.vue'
-  export default{
+    export default{
     components:{
       Button
-    }
+    },
+      setup(){
+      const onClick = ()=>{
+        console.log('hi')
+      }
+      }
   }
 </script>
 
