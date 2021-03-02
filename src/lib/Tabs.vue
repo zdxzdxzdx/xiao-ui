406f83465d6c53699c1bd0fdd34f2bb5a171219e
zdxzdxzdx <769371987@qq.com>
2021-03-02 14:39:03 +0800

    Tabs css1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+    <div class="gulu-tabs">
+        <div class="gulu-tabs-nav">
+            <div class="gulu-tabs-nav-item"
+                 v-for="(t,index) in titles" :key="index">{{t}}</div>
+        </div>
+        <component class="gulu-tabs-content-item"
+                   v-for="(c,index) in defaults" :is="c" :key="index"/>
     </div>
 </template>
 
@@ -8,11 +13,13 @@
 
   import Tab from './Tab.vue';
   export default {
+
     setup(props,context){
       const defaults = context.slots.default()
       console.log(defaults)
       console.log(defaults[0].type=== Tab)
       defaults.forEach((tag)=>{
+        console.log(tag)
         if (tag.type!==Tab){
           throw new Error('Tabs子标签必须是Tab')
         }
@@ -20,7 +27,7 @@
       const titles= defaults.map((tag)=>{
         return tag.props.title
       })
-
+      console.log(titles);
       return {defaults,titles}
     }
 
@@ -30,5 +37,30 @@
 
 
 <style scoped lang="scss">
+    $blue: #40a9ff;
+    $color: #333;
+    $border-color: #d9d9d9;
+        .gulu-tabs {
+            &-nav {
+                display: flex;
+                color: $color;
+                border-bottom: 1px solid $border-color;
+                &-item {
+                    padding: 8px 0;
+                    margin: 0 16px;
+                    cursor: pointer;
+                    &:first-child {
+                        margin-left: 0;
+                    }
+                    &.selected {
+                        color: $blue;
+                    }
+                }
+            }
+            &-content {
+                padding: 8px 0;
+            }
+        }
+
 
 </style>
