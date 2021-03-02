1abe87ab0f733d314ef59497031cd36dac4f45be
zdxzdxzdx <769371987@qq.com>
2021-03-02 16:07:48 +0800

    indicator 样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,52 +2,54 @@
     <div class="gulu-tabs">
         <div class="gulu-tabs-nav">
             <div class="gulu-tabs-nav-item" :class="{selected:t===selected}"
-                 v-for="(t,index) in titles" :key="index"  @click="select(t)">{{t}}</div>
+                 v-for="(t,index) in titles" :key="index" @click="select(t)">{{t}}
+            </div>
+            <div class="gulu-tabs-nav-indicator"></div>
         </div>
         <component class="gulu-tabs-content-item"
                    :class="{selected: c.props.title === selected }"
-                   v-for="c in defaults" :is="c" />
-    </div>
+                   v-for="c in defaults" :is="c"/>
+        </div>
 </template>
 
 <script lang="ts">
 
   import Tab from './Tab.vue';
+
   export default {
-    props:{
+    props: {
       selected: {
         type: String
       }
     },
 
-    setup(props,context){
-      const defaults = context.slots.default()
-      console.log(defaults)
-      console.log(defaults[0].type=== Tab)
-      defaults.forEach((tag)=>{
-        console.log(tag)
-        if (tag.type!==Tab){
-          throw new Error('Tabs子标签必须是Tab')
+    setup(props, context) {
+      const defaults = context.slots.default();
+      console.log(defaults);
+      console.log(defaults[0].type === Tab);
+      defaults.forEach((tag) => {
+        console.log(tag);
+        if (tag.type !== Tab) {
+          throw new Error('Tabs子标签必须是Tab');
         }
-      })
-      const titles= defaults.map((tag)=>{
-        return tag.props.title
-      })
+      });
+      const titles = defaults.map((tag) => {
+        return tag.props.title;
+      });
 
       const select = (title: string) => {
-        context.emit('update:selected', title)
-      }
+        context.emit('update:selected', title);
+      };
       console.log(titles);
 
-      return {defaults,titles,select}
+      return {defaults, titles, select};
     }
-
-  }
+  };
 
 </script>
 
 
-<style  lang="scss">
+<style lang="scss">
     $blue: #40a9ff;
     $color: #333;
     $border-color: #d9d9d9;
@@ -56,22 +58,35 @@
             display: flex;
             color: $color;
             border-bottom: 1px solid $border-color;
+            position: relative;
             &-item {
                 padding: 8px 0;
                 margin: 0 16px;
                 cursor: pointer;
+
                 &:first-child {
                     margin-left: 0;
                 }
+
                 &.selected {
                     color: $blue;
                 }
             }
+            &-indicator {
+                position: absolute;
+                height: 3px;
+                background: $blue;
+                left: 0;
+                bottom: -1px;
+                width: 100px;
+            }
         }
+
         &-content {
             padding: 8px 0;
             &-item {
                 display: none;
+
                 &.selected {
                     display: block;
                 }
