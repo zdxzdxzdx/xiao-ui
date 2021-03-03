e8e70747368889fc8bebc71abd6d3d08cb3a9c8b
zdxzdxzdx <769371987@qq.com>
2021-03-03 15:18:27 +0800

    简单高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -51,6 +51,9 @@
 </script>
 
 <style scoped lang="scss">
+     .router-link-active{
+         text-decoration: underline;
+     }
     .layout {
         display: flex;
         flex-direction: column;
