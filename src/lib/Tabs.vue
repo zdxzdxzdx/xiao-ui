de521a6541076263617afaa4476f505315ba8ec7
zdxzdxzdx <769371987@qq.com>
2021-03-03 09:34:34 +0800

    使用current 不能切换indicator

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,70 +1,83 @@
 <template>
     <div class="gulu-tabs">
         <div class="gulu-tabs-nav" ref="container">
-            <div class="gulu-tabs-nav-item" :class="{selected:t===selected}"
-                 v-for="(t,index) in titles" :key="index" @click="select(t)"
-                 :ref="el=>{if (t===selected) selectedItem = el}">{{t}}
-            </div>
-            <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+            <div class="gulu-tabs-nav-item" v-for="(t,index) in titles"
+                 :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)"
+                 :class="{selected: t=== selected}"
+                 :key="index">{{t}}</div>
+            <div class="gulu-tabs-nav-indicator"  ref="indicator">aaaaa</div>
         </div>
+
+<!--        <div class="gulu-tabs-content">-->
+<!--            <component :is="current" :key="current.props.title" />-->
+<!--        </div>-->
         <component class="gulu-tabs-content-item"
                    :class="{selected: c.props.title === selected }"
                    v-for="c in defaults" :is="c"/>
     </div>
 </template>
-<script lang="ts">
-  import Tab from './Tab.vue';
-  import {ref, onMounted,  watchEffect} from 'vue';
 
+<script lang="ts">
+  import Tab from './Tab.vue'
+  import {
+    computed,
+    ref,
+    watchEffect,
+    onMounted
+  } from 'vue'
   export default {
     props: {
       selected: {
         type: String
       }
     },
-
     setup(props, context) {
-      const selectedItem = ref<HTMLDivElement>(null);
-      const indicator = ref<HTMLDivElement>(null);
-      const container = ref<HTMLDivElement>(null);
-
-      onMounted(()=> {
-        watchEffect( () => {
-          const {width} = selectedItem.value.getBoundingClientRect();
-          indicator.value.style.width = width + 'px';
-          const {left: left1} = container.value.getBoundingClientRect();
-          const {left: left2} = selectedItem.value.getBoundingClientRect();
-          const left = left2 - left1;
-          indicator.value.style.left = left + 'px';
-
+      const selectedItem = ref < HTMLDivElement > (null)
+      const indicator = ref< HTMLDivElement > (null)
+      const container = ref < HTMLDivElement > (null)
+      onMounted(() => {
+        watchEffect(() => {
+          const {
+            width
+          } = selectedItem.value.getBoundingClientRect()
+          indicator.value.style.width = width + 'px'
+          const {
+            left: left1
+          } = container.value.getBoundingClientRect()
+          const {
+            left: left2
+          } = selectedItem.value.getBoundingClientRect()
+          const left = left2 - left1
+          indicator.value.style.left = left + 'px'
         })
       })
-
-
-
-      const defaults = context.slots.default();
-      console.log(defaults);
-      console.log(defaults[0].type === Tab);
+      const defaults = context.slots.default()
       defaults.forEach((tag) => {
-        console.log(tag);
         if (tag.type !== Tab) {
-          throw new Error('Tabs子标签必须是Tab');
+          throw new Error('Tabs 子标签必须是 Tab')
         }
-      });
+      })
+      const current = computed(() => {
+        return defaults.find(tag => tag.props.title === props.selected)
+      })
       const titles = defaults.map((tag) => {
-        return tag.props.title;
-      });
-
+        return tag.props.title
+      })
       const select = (title: string) => {
-        context.emit('update:selected', title);
-      };
-
-      return {defaults, titles, select, indicator, container, selectedItem};
+        context.emit('update:selected', title)
+      }
+      return {
+        current,
+        defaults,
+        titles,
+        select,
+        selectedItem,
+        indicator,
+        container
+      }
     }
-  };
-
+  }
 </script>
-
 
 <style lang="scss">
     $blue: #40a9ff;
@@ -76,21 +89,17 @@
             color: $color;
             border-bottom: 1px solid $border-color;
             position: relative;
-
             &-item {
                 padding: 8px 0;
                 margin: 0 16px;
                 cursor: pointer;
-
                 &:first-child {
                     margin-left: 0;
                 }
-
                 &.selected {
                     color: $blue;
                 }
             }
-
             &-indicator {
                 position: absolute;
                 height: 3px;
@@ -101,19 +110,14 @@
                 transition: all 250ms;
             }
         }
-
         &-content {
             padding: 8px 0;
-
-            &-item {
+            &-item{
                 display: none;
-
-                &.selected {
+                &.selected{
                     display: block;
                 }
             }
         }
     }
-
-
 </style>
