2d8c36ca3020020c6fce33666999a1d733db0601
zdxzdxzdx <769371987@qq.com>
2021-03-04 10:11:05 +0800

    Prism高亮源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -12,7 +12,8 @@
                     <Button>查看代码</Button>
                 </div>
                 <div class="demo-code">
-                    <pre >{{Switch1Demo.__sourceCode}}</pre>
+                    <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html, 'html')">
+                    </pre>
                 </div>
             </div>
             <div class="demo">
@@ -24,7 +25,9 @@
                     <Button>查看代码</Button>
                 </div>
                 <div class="demo-code">
-                    <pre>{{Switch2Demo.__sourceCode}}</pre>
+                    <pre class="language-css"
+                         v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html, 'html')">
+                    </pre>
                 </div>
             </div>
         </div>
@@ -38,14 +41,17 @@
     import Button from '../lib/Button.vue';
     import Switch1Demo from './Switch1.demo.vue'
     import Switch2Demo from './Switch2.demo.vue'
-
+    import 'prismjs';
+    // import 'prismjs/themes/prism.css'
+    import 'prismjs/themes/prism-okaidia.css'
+     const Prism = (window as any).Prism
     export default{
     components:{
       Button,
     },
     setup(){
       const bool=ref(false)
-      return {bool,Switch1Demo,Switch2Demo}
+      return {bool,Switch1Demo,Switch2Demo,Prism}
     }
   }
 </script>
