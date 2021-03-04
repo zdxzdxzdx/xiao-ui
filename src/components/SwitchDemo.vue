6093227bb92623eb918e174afb88e9ab58900a1c
zdxzdxzdx <769371987@qq.com>
2021-03-04 10:43:55 +0800

    封装Demo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,35 +1,9 @@
 <template>
     <div>
-
         <div>
             <h1>Switch 组件示例 </h1>
-            <div class="demo">
-                <h2>常规用法</h2>
-                <div class="demo-component">
-                    <component :is="Switch1Demo"/>
-                </div>
-                <div class="demo-actions">
-                    <Button>查看代码</Button>
-                </div>
-                <div class="demo-code">
-                    <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html, 'html')">
-                    </pre>
-                </div>
-            </div>
-            <div class="demo">
-                <h2>支持 disabled </h2>
-                <div class="demo-component">
-                    <component :is="Switch2Demo"/>
-                </div>
-                <div class="demo-actions">
-                    <Button>查看代码</Button>
-                </div>
-                <div class="demo-code">
-                    <pre class="language-css"
-                         v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html, 'html')">
-                    </pre>
-                </div>
-            </div>
+            <Demo :component="Switch1Demo"/>
+            <Demo :component="Switch2Demo"/>
         </div>
 
     </div>
@@ -38,16 +12,16 @@
 <script lang="ts">
 
     import { ref } from 'vue'
-    import Button from '../lib/Button.vue';
     import Switch1Demo from './Switch1.demo.vue'
     import Switch2Demo from './Switch2.demo.vue'
     import 'prismjs';
-    // import 'prismjs/themes/prism.css'
     import 'prismjs/themes/prism-okaidia.css'
+    import Demo from './Demo.vue';
      const Prism = (window as any).Prism
+
     export default{
     components:{
-      Button,
+      Demo,
     },
     setup(){
       const bool=ref(false)
