3c27ea8593fb2b4c4ceb22780a0208ab814f7d0c
zdxzdxzdx <769371987@qq.com>
2021-03-04 10:54:20 +0800

    查看代码是否可见

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,38 +1,48 @@
 <template>
 
-        <div class="demo">
-            <h2>{{component.__sourceCodeTitle}}</h2>
-            <div class="demo-component">
-                <component :is="component"/>
-            </div>
-
-            <div class="demo-actions">
-                <Button>查看代码</Button>
-            </div>
-            <div class="demo-code">
-                <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')" />
-            </div>
+    <div class="demo">
+        <h2>{{component.__sourceCodeTitle}}</h2>
+        <div class="demo-component">
+            <component :is="component"/>
         </div>
+
+        <div class="demo-actions">
+            <Button @click="toggleCode">查看代码</Button>
+        </div>
+        <div class="demo-code" v-if="codeVisible">
+            <pre class="language-html" v-html=html></pre>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-    import Button from '../lib/Button.vue';
+  import Button from '../lib/Button.vue';
   import 'prismjs';
-  import 'prismjs/themes/prism-okaidia.css'
-  const Prism = (window as any).Prism
+  import 'prismjs/themes/prism-okaidia.css';
+  import {ref, computed} from 'vue';
+
+  const Prism = (window as any).Prism;
   export default {
-       components:{
-         Button
-       },
-        props:{
-          component:Object
-        },
-        setup(){
-          return {
-            Prism
-          }
-        }
-  }
+    components: {
+      Button
+    },
+    props: {
+      component: Object
+    },
+    setup(props) {
+      const codeVisible = ref(false);
+      const html = computed(() => {
+        return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html');
+      });
+      const toggleCode = () => {
+        codeVisible.value = !codeVisible.value;
+      };
+
+      return {
+        Prism, html, codeVisible, toggleCode
+      };
+    }
+  };
 </script>
 
 <style scoped lang="scss">
@@ -40,22 +50,27 @@
     .demo {
         border: 1px solid $border-color;
         margin: 16px 0 32px;
-        >h2 {
+
+        > h2 {
             font-size: 20px;
             padding: 8px 16px;
             border-bottom: 1px solid $border-color;
         }
+
         &-component {
             padding: 16px;
         }
+
         &-actions {
             padding: 8px 16px;
             border-top: 1px dashed $border-color;
         }
+
         &-code {
             padding: 8px 16px;
             border-top: 1px dashed $border-color;
-            >pre {
+
+            > pre {
                 line-height: 1.1;
                 font-family: Consolas, 'Courier New', Courier, monospace;
                 margin: 0;
