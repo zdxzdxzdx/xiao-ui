dfd484de7eace0c10045422d645172bec4c4eaae
zdxzdxzdx <769371987@qq.com>
2021-03-05 14:23:04 +0800

    vue3 使用script setup

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,10 @@
    <router-view/>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import {ref, provide} from 'vue'
 import {router} from './router';
 
-export default {
-  name: 'App',
-  setup() {
      const width = document.documentElement.clientWidth;
      const menuVisible = ref(width <= 500 ? false : true)
      provide('menuVisible', menuVisible)
@@ -18,6 +15,5 @@ export default {
         }
      })
 
-  }
-}
+
 </script>
