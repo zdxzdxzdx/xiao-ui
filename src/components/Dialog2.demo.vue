6abfa87d3bc91c644393ed261cf2c45b10b65329
zdxzdxzdx <769371987@qq.com>
2021-03-05 16:21:54 +0800

    update

--- src/components/Dialog2.demo.vue
+++ src/components/Dialog2.demo.vue
@@ -7,8 +7,7 @@
 </template>
 
 <script lang="ts">
-    import {Button} from '../lib/index'
-    import {openDialog} from '../lib/index'
+    import {Button,openDialog} from '../lib/index'
     import { h } from 'vue';
   export default {
     components:{
