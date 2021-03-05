afb99ce9c8999fd1d4a6640c85901b097c65065c
zdxzdxzdx <769371987@qq.com>
2021-03-05 14:15:32 +0800

    优化代码

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,16 @@ import {router} from './router';
 
 export default {
   name: 'App',
-  setup(){
+  setup() {
      const width = document.documentElement.clientWidth;
-     const menuVisible = ref(width<=500?false:true)
-     provide('menuVisible',menuVisible)
-     router.afterEach(()=>{
-        if (width<=500){
-           menuVisible.value=false
+     const menuVisible = ref(width <= 500 ? false : true)
+     provide('menuVisible', menuVisible)
+     router.afterEach(() => {
+        if (width <= 500) {
+           menuVisible.value = false
         }
      })
 
-
+  }
 }
 </script>
